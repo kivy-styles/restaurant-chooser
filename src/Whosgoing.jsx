4174d6e2a71ecc64017bdc82a65a3e518ede371e
kivy-styles <kieranjoseph682@gmail.com>
2022-08-02 22:28:33 +0100

    ...

--- src/Whosgoing.jsx
+++ src/Whosgoing.jsx
@@ -462,13 +462,12 @@ export function Alldone(){
             setDone(theone)
         }
         return ()=>{
-            window.addEventListener("unload", function (){
-                window.localStorage.removeItem('theone')
-                window.localStorage.removeItem('People')
-                window.localStorage.removeItem('Restaurant')
-                window.localStorage.removeItem('chosenRestaurants')
-                window.localStorage.removeItem('Participants')
-                alert()})
+            window.localStorage.removeItem('theone')
+            window.localStorage.removeItem('People')
+            window.localStorage.removeItem('Restaurants')
+            window.localStorage.removeItem('chosenRestaurants')
+            window.localStorage.removeItem('Participants')
+            alert()
         }
     },[])
     return(
